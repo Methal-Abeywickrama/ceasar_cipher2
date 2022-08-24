9564a99b3147f3a2d7ee8d5f54b759bde76026b4
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2022-08-24 18:01:04 +0530

    First test written

--- spec/my_spec.rb
+++ spec/my_spec.rb
@@ -1,2 +1,9 @@
 require './lib/script'
 
+describe 'ceasar_cipher' do
+  it 'Returns a ciphered string when an input is given' do
+    input_string = "What a string!"
+    expect(ceasar_cipher(input_string, 5)).to eql("Bmfy f xywnsl!")
+  end
+end
+
